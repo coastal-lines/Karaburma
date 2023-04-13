1ed4d59859a3b57ee963f5d6454a8dc0116c79f6
coastal-lines <70205794+coastal-lines@users.noreply.github.com>
2023-04-13 07:13:11 +0200

    ''

---
+++ lena/utils/logging_manager.py
@@ -0,0 +1,32 @@
+import inspect
+from loguru import logger
+
+class LoggingManager():
+
+    def __prepare_callstack(self):
+        current_callstack_list = []
+
+        for frame_info in reversed(inspect.stack()):
+            if("lena" in frame_info.filename and "<module>" not in frame_info.function and "logging_manager" not in frame_info.filename):
+                current_callstack_list.append("method '{}' was called from '{}'".format(frame_info.function, frame_info.filename.split("\\")[-1]))
+
+        return current_callstack_list
+
+    @staticmethod
+    def log_number_arguments_error(excpected_value, actual_value):
+        current_callstack_list = LoggingManager().__prepare_callstack()
+        callstack_message = "Callstack: " + "\n".join(current_callstack_list)
+        error_message = f"{current_callstack_list[-1]} expects {excpected_value} arguments but was {actual_value}"
+        logger.error(error_message + "\n" + callstack_message)
+
+        return error_message
+
+    @staticmethod
+    def log_error(orig_error_message):
+        current_callstack_list = LoggingManager().__prepare_callstack()
+        callstack_message = "Callstack: " + "\n".join(current_callstack_list)
+        logger.error(orig_error_message  + "\n" + callstack_message)
+
+    @staticmethod
+    def log_information(message):
+        logger.info(message)
