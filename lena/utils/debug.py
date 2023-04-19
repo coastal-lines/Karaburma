a880b1bcc9260ce29af9e391cc3fc37b4d23907d
coastal-lines <70205794+coastal-lines@users.noreply.github.com>
2023-04-19 07:26:08 +0200

    ''

--- lena/utils/debug.py
+++ lena/utils/debug.py
@@ -54,3 +54,33 @@ def draw_pattern_matching_elements(screenshot_copy_debug, elements, rectangle_co
                                                                  element,
                                                                  rectangle_colour,
                                                                  rectangle_thickness)
+
+def draw_listbox(screenshot_copy_debug, screenshot_elements):
+    for listbox in (item for item in screenshot_elements.get_elements() if item.get_label() == "listbox"):
+        draw_custom_elements(screenshot_copy_debug, [listbox], "listbox", LISTBOX_COLOUR, LISTBOX_THICKNES)
+
+        if(listbox.get_v_scroll() != None):
+            draw_custom_elements(screenshot_copy_debug, [listbox.get_v_scroll()], "v_scroll", LISTBOX_COLOUR, TABLE_SCROLLS_THICKNES)
+            draw_custom_elements(screenshot_copy_debug, [listbox.get_v_scroll().get_first_button()], "UP", LISTBOX_COLOUR, TABLE_SCROLLS_THICKNES)
+            draw_custom_elements(screenshot_copy_debug, [listbox.get_v_scroll().get_second_button()], "DOWN", LISTBOX_COLOUR, TABLE_SCROLLS_THICKNES)
+
+def draw_tables(screenshot_copy_debug, screenshot_elements):
+    for table in screenshot_elements.get_table_elements():
+        draw_custom_elements(screenshot_copy_debug, screenshot_elements.get_elements(), "table", TABLE_COLOUR, TABLE_THICKNES)
+
+        if(table.get_v_scroll() != None):
+            draw_custom_elements(screenshot_copy_debug, [table.get_v_scroll()], "v_scroll", TABLE_SCROLLS_COLOUR, TABLE_SCROLLS_THICKNES)
+            draw_custom_elements(screenshot_copy_debug, [table.get_v_scroll().get_first_button()], "UP", TABLE_SCROLLS_COLOUR, TABLE_SCROLLS_THICKNES)
+            draw_custom_elements(screenshot_copy_debug, [table.get_v_scroll().get_second_button()], "DOWN", TABLE_SCROLLS_COLOUR, TABLE_SCROLLS_THICKNES)
+
+        if (table.get_h_scroll() != None):
+            draw_custom_elements(screenshot_copy_debug, [table.get_h_scroll()], "h_scroll", TABLE_SCROLLS_COLOUR, TABLE_SCROLLS_THICKNES)
+            draw_custom_elements(screenshot_copy_debug, [table.get_h_scroll().get_first_button()], "LEFT", TABLE_SCROLLS_COLOUR, TABLE_SCROLLS_THICKNES)
+            draw_custom_elements(screenshot_copy_debug, [table.get_h_scroll().get_second_button()], "RIGHT", TABLE_SCROLLS_COLOUR, TABLE_SCROLLS_THICKNES)
+
+        # draw custom cell
+        #draw_custom_elements(screenshot_copy_debug, [table.get_cell_by_adress(1, 2)], "table_cell", TABLE_COLOUR, TABLE_SCROLLS_THICKNES)
+
+        # draw all cells
+        #for cell in table.get_cells_area_element().get_list_cells():
+        #    contours_helper.DrawRectangleByListXYWH(screenshot_copy_debug, [cell.get_roi_element().get_element_features()], TABLE_CELLS_COLOUR, TABLE_CELLS_THICKNES)
