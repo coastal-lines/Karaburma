73b847df0dbded609b0596903bf8fa5a51ddc5fd
coastal-lines <70205794+coastal-lines@users.noreply.github.com>
2023-06-05 20:58:21 +0200

    'utils'

--- lena/utils/image_processing/orb_descriptor.py
+++ lena/utils/image_processing/orb_descriptor.py
@@ -18,3 +18,20 @@ def prepare_keypoints_and_descriptors_by_ORB(query_img):
     queryKeypoints, queryDescriptors = orb.detectAndCompute(query_img_bw, None)
 
     return queryKeypoints, queryDescriptors
+
+def prepare_matches_by_BFMatcher(queryDescriptors, trainDescriptors):
+    # Full bust variables - BFMatcher
+    # cv2.NORM_HAMMING - metric for comparing distance between objects
+    # crosscheck - object A will be compared with object B only if B is the nearest neighbor of A (this is longer but more accurately)
+    matcher = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+    matches = matcher.match(queryDescriptors, trainDescriptors)
+    # do sort by distance
+    matches = sorted(matches, key=lambda x: x.distance)
+    print(len(matches))
+    return matches
+
+def show_ORB_matches_between_two_images(query_img, queryKeypoints, train_img, trainKeypoints, matches):
+    final_img = cv2.drawMatches(query_img, queryKeypoints, train_img, trainKeypoints, matches[:10], query_img)
+    final_img = cv2.resize(final_img, (2000, 1000))
+    cv2.imshow("Matches", final_img)
+    cv2.waitKey(0)
