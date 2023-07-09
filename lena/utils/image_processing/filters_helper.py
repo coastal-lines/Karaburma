0ec53c5b9f8e475df07c506f326baae7aa3f3c31
coastal-lines <70205794+coastal-lines@users.noreply.github.com>
2023-07-09 12:38:07 +0200

    'utils'

--- lena/utils/image_processing/filters_helper.py
+++ lena/utils/image_processing/filters_helper.py
@@ -112,3 +112,90 @@ def threshold(img, min, max):
 def adaptive_threshold(img, max_value, block_size, constant):
     th = cv2.adaptiveThreshold(img ,max_value ,cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY ,block_size ,constant)
     return th
+
+def BlendedThreshold(img):
+    ret, th1 = cv2.threshold(img, 150, 255, cv2.THRESH_BINARY)
+    th2 = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 8)
+    blended = cv2.addWeighted(src1=th1, alpha=0.6, src2=th2, beta=0.4, gamma=0)
+    return blended
+
+def try_threshold(grayscale_image, block_size_min=3, block_size_max=11):
+    #local_thresh = threshold_local(grayscale_image, block_size=block_size, offset=3)
+
+    binary_local = []
+
+    for i in range(block_size_min, block_size_max, 2):
+        try:
+            binary_local.append(skimage.img_as_ubyte(grayscale_image > threshold_local(grayscale_image, block_size=i, offset=3)))
+        except:
+            binary_local.append(np.zeros((255,255), dtype=int))
+
+    try:
+        mean_binary = skimage.img_as_ubyte(grayscale_image.copy() > threshold_mean(grayscale_image))
+    except:
+        mean_binary = np.zeros((255,255), dtype=int)
+
+    try:
+        minimum_binary = skimage.img_as_ubyte(grayscale_image.copy() > threshold_minimum(grayscale_image))
+    except:
+        minimum_binary = np.zeros((255,255), dtype=int)
+
+    try:
+        otsu_binary = skimage.img_as_ubyte(grayscale_image.copy() > threshold_otsu(grayscale_image))
+    except:
+        otsu_binary= np.zeros((255, 255), dtype=int)
+
+    try:
+        li_binary = skimage.img_as_ubyte(grayscale_image.copy() > threshold_li(grayscale_image))
+    except:
+        li_binary = np.zeros((255, 255), dtype=int)
+
+    try:
+        isodata_binary = skimage.img_as_ubyte(grayscale_image.copy() > threshold_isodata(grayscale_image))
+    except:
+        isodata_binary = np.zeros((255, 255), dtype=int)
+
+    try:
+        triangle_binary = skimage.img_as_ubyte(grayscale_image.copy() > threshold_triangle(grayscale_image))
+    except:
+        triangle_binary = np.zeros((255, 255), dtype=int)
+
+    try:
+        yen_binary = skimage.img_as_ubyte(grayscale_image.copy() > threshold_yen(grayscale_image))
+    except:
+        yen_binary = np.zeros((255, 255), dtype=int)
+
+    '''
+    f, axarr = plt.subplots(3, 3)
+
+    axarr[0, 0].imshow(isodata_binary, cmap='gray')
+    axarr[0, 0].set_title("Isodata")
+
+    axarr[0, 1].imshow(minimum_binary, cmap='gray')
+    axarr[0, 1].set_title("Minimum")
+
+    axarr[0, 2].imshow(binary_local[0], cmap='gray')
+    axarr[0, 2].set_title("Binary")
+
+    axarr[1, 0].imshow(mean_binary, cmap='gray')
+    axarr[1, 0].set_title("Mean")
+
+    axarr[1, 1].imshow(otsu_binary, cmap='gray')
+    axarr[1, 1].set_title("Otsu")
+
+    axarr[1, 2].imshow(grayscale_image, cmap='gray')
+    axarr[1, 2].set_title("Image")
+
+    axarr[2, 0].imshow(triangle_binary, cmap='gray')
+    axarr[2, 0].set_title("Triangle")
+
+    axarr[2, 1].imshow(yen_binary, cmap='gray')
+    axarr[2, 1].set_title("Yen")
+
+    axarr[2, 2].imshow(li_binary, cmap='gray')
+    axarr[2, 2].set_title("li_binary")
+
+    plt.show()
+    '''
+
+    return binary_local, mean_binary, minimum_binary, otsu_binary, li_binary, isodata_binary, triangle_binary, yen_binary
