249d9d558130c20b9ae247ea8acb217abb9255cd
coastal-lines <70205794+coastal-lines@users.noreply.github.com>
2023-07-25 07:32:25 +0200

    'utils'

--- lena/navigation/find_manager.py
+++ lena/navigation/find_manager.py
@@ -9,3 +9,15 @@ class FindManager(TemplateMatchingElement):
     def __init__(self, detection_mode: BaseFindActions):
         super().__init__()
         self.__detection_mode = detection_mode
+
+    def __create_image_source(self, *args):
+        if len(args) == 0:
+            return ImageSourceObject(general_helpers.do_screenshot())
+        else:
+            return ImageSourceObject(files_helper.load_image(args[0]))
+
+    def find_all_elements(self, *args):
+        image_source = self.__create_image_source(*args)
+        self.__detection_mode.find_all_elements(image_source)
+
+        return image_source
