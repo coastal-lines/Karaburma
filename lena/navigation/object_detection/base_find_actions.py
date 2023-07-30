5841b251420aeec7edf11f22c1eb9d4af0dec7cd
coastal-lines <70205794+coastal-lines@users.noreply.github.com>
2023-07-30 06:49:17 +0200

    'utils'

---
+++ lena/navigation/object_detection/base_find_actions.py
@@ -0,0 +1,15 @@
+from abc import ABC, abstractmethod
+
+
+class BaseFindActions(ABC):
+
+    def get_source_image(self):
+        pass
+
+    @abstractmethod
+    def find_all_elements(self, screenshot):
+        pass
+
+    @abstractmethod
+    def find_element(self, screenshot, element_type):
+        pass
