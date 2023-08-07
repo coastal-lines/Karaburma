746bc40fc92c47f169a2b78fe8bc44ac60249da7
coastal-lines <70205794+coastal-lines@users.noreply.github.com>
2023-08-07 17:08:05 +0200

    'utils'

---
+++ lena/navigation/object_detection/custom_actions/custom_find_actions.py
@@ -0,0 +1,6 @@
+from lena.data.constants.enums.element_types_enum import ElementTypesEnum
+from lena.navigation.object_detection.base_find_actions import BaseFindActions
+
+
+class CommonMlFindActions():
+    pass
