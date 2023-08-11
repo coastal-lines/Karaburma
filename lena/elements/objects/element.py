196d171322e7246eb0bb058ba6f05fd6c0555220
coastal-lines <70205794+coastal-lines@users.noreply.github.com>
2023-08-11 19:30:44 +0200

    'utils'

---
+++ lena/elements/objects/element.py
@@ -0,0 +1,11 @@
+from lena.elements.objects.roi_element import RoiElement
+
+
+class Element:
+    def __init__(self, label: str, prediction_value: float, roi_element: RoiElement):
+        self.__label = label
+        self.__prediction_value = str("%.2f" % prediction_value)
+        self.__roi_element = roi_element
+
+    def get_label(self) -> str:
+        return self.__label
