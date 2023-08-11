efee01225482952864f411b2c98f7765b3de4c2a
coastal-lines <70205794+coastal-lines@users.noreply.github.com>
2023-08-11 20:03:19 +0200

    'utils'

--- lena/elements/objects/element.py
+++ lena/elements/objects/element.py
@@ -12,3 +12,14 @@ class Element:
 
     def get_prediction_value(self) -> str:
         return self.__prediction_value
+
+    def get_roi_element(self) -> RoiElement:
+        return self.__roi_element
+
+    def get_centroid(self) -> tuple:
+        return (int(self.get_roi_element().get_x() + (self.get_roi_element().get_w() // 2)),
+                int(self.get_roi_element().get_y() + (self.get_roi_element().get_h() // 2)))
+
+    def update_prediction_value(self, prediction_value: str):
+        __prediction_value = str("%.2f" % prediction_value)
+        
