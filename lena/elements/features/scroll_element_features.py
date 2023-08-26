e4b1233ac1e65e061776c2bfa5b31e39f6de7d05
coastal-lines <70205794+coastal-lines@users.noreply.github.com>
2023-08-26 08:00:53 +0200

    'features'

---
+++ lena/elements/features/scroll_element_features.py
@@ -0,0 +1,20 @@
+import cv2
+import numpy as np
+import pyautogui
+
+from lena.data.constants.enums.element_types_enum import ElementTypesEnum
+from lena.data.constants.enums.scroll_direction_enum import ScrollDirectionEnum
+from lena.utils import general_helpers, files_helper
+from lena.elements.objects.element import Element
+from lena.elements.objects.scroll_element import ScrollElement
+from lena.utils.image_processing import filters_helper, geometric_transformations, contours_helper, pattern_matching
+from lena.elements.objects.roi_element import RoiElement
+
+class TempScrollAttributes:
+    def __init__(self, sorted_rectangles, index):
+        self.current_x, self.current_y, self.current_w, self.current_h = contours_helper.extract_xywh(sorted_rectangles, index)
+        self.current_center_x = contours_helper.get_x_center(self.current_x, self.current_w)
+        self.current_center_y = contours_helper.get_y_center(self.current_y, self.current_h)
+        self.next_x, self.next_y, self.next_w, self.next_h = contours_helper.extract_xywh(sorted_rectangles, index + 1)
+        self.next_center_x = contours_helper.get_x_center(self.next_x, self.next_w)
+        self.next_center_y = contours_helper.get_y_center(self.next_y, self.next_h)
