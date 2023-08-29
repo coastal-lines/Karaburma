8039a1957ab65ca0d30035e88a721fcfece6413a
coastal-lines <70205794+coastal-lines@users.noreply.github.com>
2023-08-29 19:49:37 +0200

    'features'

--- lena/elements/features/scroll_element_features.py
+++ lena/elements/features/scroll_element_features.py
@@ -50,3 +50,43 @@ class ScrollElementDetectionsFeatures():
         filtered_rectangles = contours_helper.get_contours_after_approximation(thresh, 3)
 
         return filtered_rectangles
+
+    def __prepare_rectangle_for_vertical_scroll(self, temp_attributes, y_max, y_min):
+        x = np.min([temp_attributes.current_x, temp_attributes.next_x]) - self.__shift_threshold_for_scrolls
+        y = y_min - self.__shift_threshold_for_scrolls
+        w = temp_attributes.current_w + self.__shift_threshold_for_scrolls
+        h = temp_attributes.current_h + self.__shift_threshold_for_scrolls + (y_max - y_min)
+        return x, y, w, h
+
+    def __prepare_rectangle_for_horizontal_scroll(self, temp_attributes, x_max, x_min):
+        x = x_min - self.__shift_threshold_for_scrolls
+        y = np.min([temp_attributes.current_y, temp_attributes.next_y]) - self.__shift_threshold_for_scrolls
+        w = temp_attributes.current_w + self.__shift_threshold_for_scrolls + (x_max - x_min)
+        h = temp_attributes.current_h + self.__shift_threshold_for_scrolls
+        return x, y, w, h
+
+    def __init_scroll_button(self, source_element, x, y, w, h, button_type, button_side):
+        button_absolute_x, button_absolute_y = general_helpers.calculate_absolute_coordinates(source_element, x, y)
+        roi_element = RoiElement(source_element.get_roi()[y:y + h, x:x + h, :], button_absolute_x, button_absolute_y, button_side, button_side)
+        return Element(button_type, 1.0, roi_element)
+
+    def __init_v_scroll(self, source_element, x, y, w, h):
+        temp_v_scroll_up_button = self.__init_scroll_button(source_element, x, y, w, h, ScrollDirectionEnum.UP.name, w)
+
+        current_down_button_y = (y + h) - w
+        temp_v_scroll_down_button = self.__init_scroll_button(source_element, x, current_down_button_y, w, h, ScrollDirectionEnum.DOWN.name, w)
+
+        return self.__init_scroll(source_element, temp_v_scroll_up_button, temp_v_scroll_down_button, x, y, w, h, ElementTypesEnum.v_scroll.name)
+
+    def __init_h_scroll(self, source_element, x, y, w, h):
+        temp_h_scroll_left_button = self.__init_scroll_button(source_element, x, y, w, h, ScrollDirectionEnum.LEFT.name, h)
+
+        current_right_button_x = (x + w) - h
+        temp_h_scroll_right_button = self.__init_scroll_button(source_element, current_right_button_x, y, w, h, ScrollDirectionEnum.RIGHT.name, h)
+
+        return self.__init_scroll(source_element, temp_h_scroll_left_button, temp_h_scroll_right_button, x, y, w, h, ElementTypesEnum.h_scroll.name)
+
+    def __init_scroll(self, source_element, first_button, second_button, x, y, w, h, scroll_type):
+        temp_scroll_absolute_x, temp_scroll_absolute_y = general_helpers.calculate_absolute_coordinates(source_element, x, y)
+        temp_scroll_roi = RoiElement(source_element.get_roi()[y:y + h, x:x + w, :], temp_scroll_absolute_x, temp_scroll_absolute_y, w, h, scroll_type)
+        return ScrollElement(scroll_type, 1.0, temp_scroll_roi, first_button, second_button)
