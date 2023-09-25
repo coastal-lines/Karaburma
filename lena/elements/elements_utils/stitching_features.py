982bd63a11beb715ff3ec0585ecab38982f0427f
coastal-lines <70205794+coastal-lines@users.noreply.github.com>
2023-09-25 12:50:48 +0200

    'features'

--- lena/elements/elements_utils/stitching_features.py
+++ lena/elements/elements_utils/stitching_features.py
@@ -43,3 +43,34 @@ class StitchingFeatures:
             case ScrollDirectionEnum.DOWN.name:
                 roi_after = temp_nested_element[self.nested_element_h - self.y_displacement: self.nested_element_h, :, :]
                 return roi_after
+
+    def right_down_stitch(self):
+        list_full_horizontal_rows = []
+
+        self.displacement_features.scroll_features.direction = ScrollDirectionEnum.RIGHT.name
+        start_full_roi = self.horizontal_stitch()
+
+        while(self.displacement_features.scroll_features.scroll_element(ScrollDirectionEnum.DOWN.name)[0]):
+            self.displacement_features.scroll_features.direction = ScrollDirectionEnum.LEFT.name
+            self.displacement_features.scroll_features.scroll_until_its_possible()
+            self.displacement_features.scroll_features.direction = ScrollDirectionEnum.RIGHT.name
+
+            #stitched_element, full_roi = self.horizontal_stitch()
+            stitched_element = self.horizontal_stitch()
+            full_horizontal_row = stitched_element[self.nested_element_h - self.y_displacement:self.nested_element_h, :, :]
+            list_full_horizontal_rows.append(full_horizontal_row)
+
+            self.displacement_features.scroll_features.direction = ScrollDirectionEnum.DOWN.name
+
+        updated_h = self.y_displacement * len(list_full_horizontal_rows)
+        full_roi = np.ones((updated_h, start_full_roi.shape[1], 3), dtype=np.uint8) * 255
+        for i in range(len(list_full_horizontal_rows)):
+            full_roi[i * self.y_displacement: (i * self.y_displacement) + self.y_displacement, :, :] = list_full_horizontal_rows[i]
+
+        stitched_element = np.ones((self.nested_element_h + updated_h, start_full_roi.shape[1], 3), dtype=np.uint8) * 255
+        stitched_element[0:self.nested_element_h, :, :] = start_full_roi[0:self.nested_element_h, :, :]
+        stitched_element[self.nested_element_h:self.nested_element_h + updated_h, :, :] = full_roi
+
+        #files_helper.save_image(stitched_element, "9999full_table")
+
+        return stitched_element
