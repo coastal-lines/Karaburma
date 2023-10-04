a05533c12c6de50c055156c765e67e70b87eb454
coastal-lines <70205794+coastal-lines@users.noreply.github.com>
2023-10-04 16:48:14 +0200

    'preprocessing'

--- lena/elements/elements_utils/preprocessing/basic_preprocessing.py
+++ lena/elements/elements_utils/preprocessing/basic_preprocessing.py
@@ -10,3 +10,17 @@ from lena.utils.image_processing import filters_helper, morphological_helpers, c
 
 
 class BasicPreprocessing:
+
+    def prepare_image(self, image_source):
+        gr = filters_helper.convert_to_grayscale(image_source.get_current_image_source())
+        sh = filters_helper.Sharp(gr, "strong")
+        er = morphological_helpers.erosion(sh)
+        th = er.copy() > sk.filters.threshold_local(er,
+                                                    block_size=ConfigManager().config.
+                                                    elements_parameters.common_element.
+                                                    preprocessing.contours_parameters["threshold_block_size"],
+                                                    offset=ConfigManager().config.elements_parameters.common_element.
+                                                    preprocessing.contours_parameters["threshold_offset"])
+        th = sk.img_as_ubyte(th)
+
+        return th
