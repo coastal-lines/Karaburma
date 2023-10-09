628f232ed58421a53dc83424b2340ab75adcc21a
coastal-lines <70205794+coastal-lines@users.noreply.github.com>
2023-10-09 10:53:16 +0200

    'preprocessing'

--- lena/elements/elements_utils/preprocessing/listbox_preprocessing.py
+++ lena/elements/elements_utils/preprocessing/listbox_preprocessing.py
@@ -19,3 +19,37 @@ class ListboxPreprocessing:
         self.__scroll_element_features = ScrollElementDetectionsFeatures(common_element_features,
                                                                          scroll_buttons_patterns,
                                                                          shift_threshold_for_scrolls)
+
+    def image_processing_for_listbox(self, image):
+        grey_ = filters_helper.convert_to_grayscale(image)
+        grey_ = filters_helper.LevelsCorrection(grey_, ConfigManager().config.elements_parameters.listbox.preprocessing["level_correction_1"])
+        grey_ = filters_helper.LevelsCorrection(grey_, ConfigManager().config.elements_parameters.listbox.preprocessing["level_correction_2"])
+        grey_ = morphological_helpers.erosion(grey_)
+        grey_ = filters_helper.LevelsCorrection(grey_, ConfigManager().config.elements_parameters.listbox.preprocessing["level_correction_3"])
+        grey_ = filters_helper.LevelsCorrection(grey_, ConfigManager().config.elements_parameters.listbox.preprocessing["level_correction_4"])
+
+        ret, grey_ = filters_helper.threshold(grey_, ConfigManager().config.elements_parameters.listbox.preprocessing["threshold_min"],
+                                              ConfigManager().config.elements_parameters.listbox.preprocessing["threshold_max"])
+
+        return grey_
+
+    def prepare_features_for_listbox(self, image):
+        grey_ = filters_helper.convert_to_grayscale(image)
+        colours = filters_helper.calculate_white_colour(grey_)
+        prepared_img = self.image_processing_for_listbox(grey_)
+
+        #general_helpers.show(prepared_img)
+
+        train_image_dimension = ConfigManager().config.elements_parameters.listbox.preprocessing["sample_dimension"]
+        resized_prepared_img = np.array(PIL_Image.fromarray(prepared_img).resize(tuple(train_image_dimension), PIL_Image.BICUBIC))
+
+        scaler = MinMaxScaler()
+        resized_prepared_img = scaler.fit_transform(resized_prepared_img)
+
+        image_features = np.array(resized_prepared_img)
+        image_features = image_features.reshape(image_features.shape[0], -1).flatten()
+
+        concatenated_features = np.concatenate((image_features, colours))
+
+        return concatenated_features
+    
