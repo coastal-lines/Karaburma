97610f1d1a74c15f8bf47bb5b6ccbe58c61e748d
coastal-lines <70205794+coastal-lines@users.noreply.github.com>
2023-10-14 20:33:34 +0200

    'preprocessing'

--- lena/elements/elements_utils/preprocessing/table_preprocessing.py
+++ lena/elements/elements_utils/preprocessing/table_preprocessing.py
@@ -16,3 +16,27 @@ from lena.utils import data_normalization, general_helpers
 
 
 class TablePreprocessing:
+    def __init__(self):
+        self.image_source = None
+
+    @property
+    def image_source(self):
+        return self.__image_source
+
+    @image_source.setter
+    def image_source(self, screenshot: ImageSourceObject):
+        print("Setting value")
+        self.__image_source = screenshot
+
+    def __resize_source_image_and_apply_filter(self, roi):
+
+        grey_ = filters_helper.convert_to_grayscale(roi)
+        imnp = np.array(grey_) / 255
+        gamma = math.log(imnp.mean()) / math.log(0.1)
+        new = ((imnp ** (1 / gamma)) * 255).astype(np.uint8)
+        new = np.array(PIL_Image.fromarray(new).resize(ConfigManager().config.elements_parameters.table["fixed_size_for_preprocessing"], PIL_Image.BICUBIC))
+        er = img_as_ubyte(morphological_helpers.erosion(new))
+        er = morphological_helpers.erosion(er)
+        dl = morphological_helpers.dilation(er)
+
+        return dl
