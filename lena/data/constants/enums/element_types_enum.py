2a41f539a1a6b6abf742c67d33ac716259cfc691
coastal-lines <70205794+coastal-lines@users.noreply.github.com>
2023-10-27 20:25:20 +0200

    'preprocessing'

---
+++ lena/data/constants/enums/element_types_enum.py
@@ -0,0 +1,14 @@
+from enum import Enum
+
+class ElementTypesEnum(Enum):
+    button = 1
+    checkbox = 2
+    combobox = 3
+    h_scroll = 4
+    v_scroll = 5
+    input = 6
+    non = 7
+    radiobutton = 8
+    slider = 9
+    table = 10
+    listbox = 11
